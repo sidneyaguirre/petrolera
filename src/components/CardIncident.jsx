39e682f9b9e430d89cd2f2847f9e8a65d6557856
sidneyaguirre <sidney.aguirrec@gmail.com>
2019-10-09 18:15:52 -0500

    styling cards

--- src/components/CardIncident.jsx
+++ src/components/CardIncident.jsx
@@ -23,50 +23,74 @@ class CardIncident extends Component {
         {this.props.data.map(incident => {
           return (
             <li key={incident.id} className="incident-element-list">
-              <div className="card rounded-lg is-box">
+              <div className="card">
                 <div className="card-header">
-                  <h5 className="card-title text-primary">{incident.title}</h5>
+                  <h5 className="card-title text-primary text-capitalize pt-2">
+                    {incident.title}
+                  </h5>
                 </div>
-                <div className="card-body">
-                  <div className="is-font-small">
-                    <strong>Estado:</strong> {incident.state}
+                <div className="card-body pb-0">
+                  <table className="table table-borderless">
+                    <thead>
+                      <tr className="p-1">
+                        <th scope="col">Estado</th>
+                        <th scope="col">Categoría</th>
+                        <th scope="col">Impacto</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr className="p-1">
+                        <td>{incident.state}</td>
+                        <td>{incident.category}</td>
+                        <td>{incident.impact}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+                  <div className="row">
+                    <div className="is-font-small ml-3 mr-3">
+                      <strong>Fecha Suceso:</strong> {incident.start_date}
+                      <br />
+                    </div>
                   </div>
-                  <div className="is-font-small">
-                    <strong>Fecha:</strong> {incident.start_date} -{" "}
-                    {incident.end_date}
+
+                  <div className="row">
+                    <div className="is-font-small ml-3 mr-3">
+                      <strong>Fecha Clausura:</strong>
+                      {incident.end_date}
+                      <br />
+                    </div>
                   </div>
-                  <div className="is-font-small">
-                    <strong>Categoría:</strong> {incident.category}
+
+                  <div className="is-font-small ml-3 mr-3 pt-2">
+                    <strong>Descripción</strong>
                   </div>
-                  <div className="is-font-small">
-                    <strong>Impacto:</strong> {incident.impact}
-                  </div>
-                  <div>
-                    <strong>Descripción:</strong>
-                  </div>
-                  <div className="card-body card-description">
+                  <div className="card-description p-3 m-0">
                     <div className="is-font-small">{incident.description}</div>
                   </div>
                 </div>
+
                 <div className="card-footer">
-                  <div className="is-font-small">
-                    <strong>Creado por:</strong> {incident.createdBy}
-                  </div>
-                  <div className="is-font-small">
-                    <strong>Responsable:</strong> {incident.assigned}
-                  </div>
-                  <div className="is-font-small">
-                    <strong>Investigador:</strong> {incident.investigator}
+                  <div className="row ml-3 mr-3">
+                    <div className="is-font-small ml-3 mr-3">
+                      <strong>Creado por:</strong> {incident.createdBy}
+                    </div>
+                    <div className="is-font-small ml-3 mr-3">
+                      <strong>Responsable:</strong> {incident.assigned}
+                    </div>
+                    <div className="is-font-small ml-3 mr-3">
+                      <strong>Investigador:</strong> {incident.investigator}
+                    </div>
                   </div>
                 </div>
-                <div>
+                <div className="pb-3">
                   <Link
                     onClick={this.handleClick}
                     to={{
                       pathname: "/edit-incident",
                       incidentInfo: { incident }
                     }}
-                    className="btn btn-primary stretched-link"
+                    className="btn btn-primary m-0"
                     hidden={this.handleEdition()}
                   >
                     Editar
